4b769e49008611c73b18b14e43c3714b392a4b5c
DREAMWORLDVOID <yshliu0321@icloud.com>
2018-06-09 14:00:46 +0800

    Arrange CMake Files

---
+++ CMake/BootStrap.cmake
@@ -0,0 +1,36 @@
+# Set Env Options
+set(CMAKE_MACOSX_RPATH ON)
+set(CMAKE_CXX_STANDARD 17)
+
+# Set Project Build Output Directories
+set(PROJECT_DIR ${CMAKE_SOURCE_DIR})
+set(SDK_DIR ${CMAKE_BINARY_DIR}/SDK$-<CONFIG>)
+set(PRODUCT_DIR ${CMAKE_BINARY_DIR}/Product-$<CONFIG>)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${SDK_DIR}/Lib)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PRODUCT_DIR})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PRODUCT_DIR})
+
+# Check IPO Support
+cmake_policy(SET CMP0069 NEW)
+include(CheckIPOSupported)
+check_ipo_supported(RESULT NWCONF_IPO_SUPPORT OUTPUT NWCONF_IPO_SUPPORT_MESSAGE)
+if (NWCONF_IPO_SUPPORT)
+    message(STATUS "IPO IS SUPPORTED, ENABLED")
+else()
+    message(STATUS "IPO IS NOT SUPPORTED: ${NWCONF_IPO_SUPPORT_MESSAGE}, DISABLED")
+endif ()
+
+function(target_enable_ipo NAME)
+    if (NWCONF_IPO_SUPPORT)
+        set_property(TARGET ${NAME} PROPERTY INTERPROCEDURAL_OPTIMIZATION TRUE)
+    endif ()
+endfunction()
+
+# Enable Filesystem
+if(UNIX OR MINGW)
+    link_libraries(stdc++fs)
+endif()
+
+# Enable Multi-threaded
+find_package(Threads REQUIRED)
+link_libraries(Threads::Threads)
---
+++ CMake/Commands.cmake
@@ -0,0 +1,47 @@
+# Install Utilities
+function(nwstd_install_include_files SOURCE_DIR)
+    install(DIRECTORY ${SOURCE_DIR}/Source/
+            DESTINATION ${CMAKE_INSTALL_PREFIX}/SDK/Include
+            FILES_MATCHING PATTERN "*.h"
+            )
+
+    if (EXISTS ${SOURCE_DIR}/3rdParty)
+        install(DIRECTORY ${SOURCE_DIR}/3rdParty/
+                DESTINATION ${CMAKE_INSTALL_PREFIX}/SDK/Include
+                )
+    endif ()
+endfunction()
+
+function(nwstd_install_targets)
+    install(TARGETS ${ARGV}
+            RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/Product-$<CONFIG>
+            LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/Product-$<CONFIG>
+            ARCHIVE DESTINATION ${CMAKE_INSTALL_PREFIX}/SDK/Lib)
+endfunction()
+
+# Standard Target Utilities
+function(nwstd_target_define_common_properties NAME DIRECTORY)
+    target_enable_ipo(${NAME})
+    # Set Target Include Directories
+    target_include_directories(${NAME} PUBLIC ${DIRECTORY}/Source)
+    nwstd_install_targets(${NAME})
+    if (EXISTS ${DIRECTORY}/3rdParty)
+        target_include_directories(${NAME} PUBLIC ${DIRECTORY}/3rdParty)
+        target_include_directories(${NAME} SYSTEM PUBLIC ${DIRECTORY}/3rdParty)
+    endif()
+    # Target Install Script
+    nwstd_install_targets(${NAME})
+    nwstd_install_include_files(${DIRECTORY})
+endfunction()
+
+function(nwstd_add_executable NAME DIRECTORY)
+    file(GLOB_RECURSE SRC DIRECTORY ${DIRECTORY}/Source/*.*)
+    add_executable(${NAME} ${SRC})
+    nwstd_target_define_common_properties(${NAME} ${DIRECTORY})
+endfunction()
+
+function(nwstd_add_library NAME TYPE DIRECTORY)
+    file(GLOB_RECURSE SRC DIRECTORY ${DIRECTORY}/Source/*.*)
+    add_library(${NAME} ${TYPE} ${SRC})
+    nwstd_target_define_common_properties(${NAME} ${DIRECTORY})
+endfunction()
